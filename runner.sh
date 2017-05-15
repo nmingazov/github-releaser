5436d89582fb03daa2fd1be816d1c25f1271fc96
nmingazov <nicrotek547@gmail.com>
2017-05-15 12:38:49 +0300

    now it also test for empty results

--- runner.sh
+++ runner.sh
@@ -2,13 +2,21 @@
 echo 'Starting to make up your release...'
 github-changes $@
 
-echo 'Following lines will be included into release:'
 file="./CHANGELOG.md"
 skip_line=1
 upcoming_regexp="^###[[:space:]]upcoming.*$"
+result=
 while read -r line; do
-    [[ "$line" =~ $upcoming_regexp ]] && skip_line=0 && continue
-    [[ $skip_line == 1 ]] && continue
-    [[ $skip_line == 0 && -z $line ]] && skip_line=1 && continue
-    echo "${line}"
-done < "$file"
+    [[ "${line}" =~ ${upcoming_regexp} ]] && skip_line=0 && continue
+    [[ ${skip_line} == 1 ]] && continue
+    [[ ${skip_line} == 0 && -z $line ]] && skip_line=1 && continue
+    result="${result}${line}"
+done < "${file}"
+
+if [[ -z ${result} ]]; then
+    echo 'Nothing to include into release. Exiting..'
+    exit
+else
+    echo 'Following lines will be included into release:'
+    echo ${result}
+fi
